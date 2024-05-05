852b0e4e1a83828f46e24d09658c4ccf46a7d293
BallOfEnergy <66693744+BallOfEnergy1@users.noreply.github.com>
2024-05-05 13:58:53 -0500

    oops :3

--- ocz-install.lua
+++ ocz-install.lua
@@ -13,7 +13,7 @@ shell.execute("mkdir " .. destdir .. "/bin")
 shell.execute("mkdir " .. destdir .. "/lib")
 shell.execute("mkdir " .. destdir .. "/lib/ocz")
 
-shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/ocz.sh " .. destdir .. "/bin/ocz.lua")
+shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/ocz.lua " .. destdir .. "/bin/ocz.lua")
 shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/init.lua " .. destdir .. "/lib/ocz/init.lua")
 shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/crc32.lua " .. destdir .. "/lib/ocz/crc32.lua")
 shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/lualzw.lua " .. destdir .. "/lib/ocz/lualzw.lua")
