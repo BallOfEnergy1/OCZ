27b89f21faea3898098e8e4f2651c76b61fb03fb
BallOfEnergy <66693744+BallOfEnergy1@users.noreply.github.com>
2024-05-09 09:36:41 -0500

    Update ocz-install.lua

--- ocz-install.lua
+++ ocz-install.lua
@@ -18,5 +18,6 @@ shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/i
 shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/crc32.lua " .. destdir .. "/lib/ocz/crc32.lua")
 shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/lualzw.lua " .. destdir .. "/lib/ocz/lualzw.lua")
 shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/md5.lua " .. destdir .. "/lib/ocz/md5.lua")
+shell.execute("wget https://raw.githubusercontent.com/BallOfEnergy1/OCZ/master/progress.lua " .. destdir .. "/lib/ocz/progress.lua")
 
 return true
